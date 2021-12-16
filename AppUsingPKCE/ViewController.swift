99195b2c1134083f207cbc0d370e744b45f75e62
Marco Eidinger <marco.eidinger@sap.com>
2021-12-16 10:06:27 -0800

    using OAuth2PKCEAuthenticator
    note: example-specific values related to Auth0 test environment

--- AppUsingPKCE/ViewController.swift
+++ AppUsingPKCE/ViewController.swift
@@ -15,12 +15,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func signIn(_ sender: Any) {
-        let alert = UIAlertController(
-            title: "Result",
-            message: "TODO",
-            preferredStyle: .alert)
-        alert.addAction(.init(title: "Ok", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        // Example-specific values
+        let bundleIdentifier = Bundle.main.bundleIdentifier!
+        let auth0domain = "dev-f78zik4c.us.auth0.com"
+        let authorizeURL = "https://\(auth0domain)/authorize"
+        let tokenURL = "https://\(auth0domain)/oauth/token"
+        let clientId = "txfpmPJryrScEL9bu0jnHT55lFokXftO"
+        let redirectUri = "\(bundleIdentifier)://\(auth0domain)/ios/\(bundleIdentifier)/callback"
+
+        // Example-agnostic code
+        let parameters = OAuth2PKCEParameters(authorizeUrl: authorizeURL,
+                                        tokenUrl:tokenURL,
+                                        clientId: clientId,
+                                        redirectUri: redirectUri,
+                                        callbackURLScheme: bundleIdentifier)
+
+        let authenticator = OAuth2PKCEAuthenticator()
+        authenticator.authenticate(parameters: parameters) { result in
+            var message: String = ""
+            switch result {
+            case .success(let accessTokenResponse):
+                message = accessTokenResponse.access_token
+            case .failure(let error):
+                message = error.localizedDescription
+            }
+
+            let alert = UIAlertController(
+                title: "Result",
+                message: message,
+                preferredStyle: .alert)
+            alert.addAction(.init(title: "Ok", style: .default, handler: nil))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
 
 }
