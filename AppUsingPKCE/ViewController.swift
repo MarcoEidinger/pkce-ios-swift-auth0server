8cd0204c2b65ce74a50d190fdae28777b7c750d5
Marco Eidinger <marco.eidinger@sap.com>
2021-12-16 07:12:47 -0800

    add signIn action and placeholder

--- AppUsingPKCE/ViewController.swift
+++ AppUsingPKCE/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func signIn(_ sender: Any) {
+        let alert = UIAlertController(
+            title: "Result",
+            message: "TODO",
+            preferredStyle: .alert)
+        alert.addAction(.init(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
 
